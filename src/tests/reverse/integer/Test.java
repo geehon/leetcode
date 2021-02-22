e11d65cc0e0fadd26b5ae3bd82fe56d3f4fbd01a
GeeHon <liaoliaoliao70@gmail.com>
2021-02-22 18:32:28 +0800

    reverse integer

---
+++ src/tests/reverse/integer/Test.java
@@ -0,0 +1,16 @@
+package tests.reverse.integer;
+
+import solution.reverse.integer.Solution01;
+
+public class Test {
+    public static void main(String[] args) {
+        // define testData num
+        int[] nums = {Integer.MIN_VALUE, 1233, 0, -1232, 214748365, Integer.MAX_VALUE};
+        Solution01 solution01 = new Solution01();
+        int tester;
+        for (int x : nums) {
+            tester = solution01.reverse(x);
+            System.out.println(tester);
+        }
+    }
+}
